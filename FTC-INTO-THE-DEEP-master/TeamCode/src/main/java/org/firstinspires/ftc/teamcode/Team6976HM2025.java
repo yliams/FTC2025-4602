164fd89eafdc1e48eb5e3206e6402b5d0af41344
Nobody <nobodyhere618@gmail.com>
2024-09-19 08:35:38 -0500

    Added SDK

---
+++ FTC-INTO-THE-DEEP-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team6976HM2025.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+
+public class Team6976HM2025 {
+    public DcMotor DriveRightBack = null;
+    public DcMotor DriveLeftBack = null;
+    public DcMotor DriveLeftFront = null;
+    public DcMotor DriveRightFront = null;
+
+
+//I fixed it
+
+
+
+    //public NormalizedColorSensor ColorSensor = null;
+
+
+
+    //public ElapsedTime period  = new ElapsedTime();
+
+    // public Team4008HMNew() {}
+
+    HardwareMap hwMap = null;
+    //FIX AND USE IT
+    public void Map(HardwareMap hardwareMap)
+    {
+        hwMap = hardwareMap;
+        DriveLeftFront = hwMap.get(DcMotor.class,"DriveLeftFront");
+        DriveRightFront = hwMap.get(DcMotor.class,"DriveRightFront");
+        DriveLeftBack = hwMap.get(DcMotor.class,"DriveLeftBack");
+        DriveRightBack = hwMap.get(DcMotor.class,"DriveRightBack");
+        //   Lights = hwMap.get(RevBlinkinLedDriver.class,"Lights");
+
+        DriveLeftFront.setDirection(DcMotor.Direction.FORWARD);
+        DriveLeftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DriveLeftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        DriveRightFront.setDirection(DcMotor.Direction.REVERSE);
+        DriveRightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DriveRightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        DriveLeftBack.setDirection(DcMotor.Direction.FORWARD);
+        DriveLeftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DriveLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        DriveRightBack.setDirection(DcMotor.Direction.REVERSE);
+        DriveRightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DriveRightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+//        Turret = hwMap.get(DcMotor.class,"Turret");
+//        Turret.setDirection(DcMotorSimple.Direction.REVERSE);
+//        Turret.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        //IntakeLift.setDirection(DcMotor.Direction.REVERSE);
+
+//        Elevator = hwMap.get(DcMotor.class, "Elevator");
+//        Elevator.setDirection(DcMotorSimple.Direction.REVERSE);
+//        Elevator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        Elevator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+
+//        Arm2 = hwMap.get(DcMotor.class, "Arm2");
+//        Arm2.setDirection(DcMotorSimple.Direction.REVERSE);
+//        Arm2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        Arm2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+//        Intake = hwMap.get(Servo.class, "Servo1");
+
+        //Arm = hwMap.get(DcMotor.class, "Arm");
+
+//        ColorSensor = hwMap.get(NormalizedColorSensor.class, "sensor");
+
+    }
+}
---
+++ FTC-INTO-THE-DEEP-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team6976TeleOp2025.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name = "Team6976TeleOp2025", group = "6976")
+public class Team6976TeleOp2025 extends LinearOpMode {
+    Team6976HM2025 robot = new Team6976HM2025();
+
+
+    @Override
+    public void runOpMode() {
+        robot.Map(hardwareMap);
+        telemetry.addData("Say", "TeleOp Starting");
+        telemetry.update();
+        robot.DriveRightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.DriveRightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.DriveLeftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.DriveLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.DriveRightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.DriveRightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.DriveLeftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        robot.Intake.setPosition(0.3);
+
+        int count = 0;
+        //robot.Lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.BEATS_PER_MINUTE_PARTY_PALETTE);
+        waitForStart();
+
+        while (opModeIsActive()) {
+            boolean speedslow = gamepad1.right_bumper;
+            double mag = speedslow ? 0.5 : 1.0;
+
+            double y = gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = -gamepad1.right_stick_x;
+
+
+          /*  if (robot.Touched.isPressed()) {
+                telemetry.addData("Touch", robot.Touched.isPressed());
+                telemetry.update();
+            } */
+
+            // hello
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1.5);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            telemetry.addData("RightFront", robot.DriveRightFront.getCurrentPosition());
+            telemetry.addData("RightBack", robot.DriveRightBack.getCurrentPosition());
+            telemetry.addData("LeftFront", robot.DriveLeftFront.getCurrentPosition());
+            telemetry.addData("LeftBack", robot.DriveLeftBack.getCurrentPosition());
+            telemetry.update();
+
+            robot.DriveLeftFront.setPower(frontLeftPower * mag);
+            robot.DriveLeftBack.setPower(backLeftPower * mag);
+            robot.DriveRightFront.setPower(frontRightPower * mag);
+            robot.DriveRightBack.setPower(backRightPower * mag);
+
+
+
+//            double Arm1 = gamepad2.right_stick_y;
+//            double Arm2 = gamepad2.left_stick_y;
+//            robot.Arm1.setPower(Arm1);
+//            robot.Arm2.setPower(Arm2);
+            // left bumper is open, right bumper is close
+//            if (gamepad2.right_bumper) {
+//                robot.Intake.setPosition(1.0);
+//            }
+//            if (gamepad2.left_bumper){
+//                robot.Intake.setPosition(0.3);
+//            }
+
+//            if(gamepad2.y){
+//                robot.Arm1.setPower(.9);
+//            }
+//            else if (gamepad2.x){
+//                robot.Arm1.setPower(-1);
+//            }
+//            else {
+//                robot.Arm1.setPower(0);
+//            }
+        }
+    }
+}
