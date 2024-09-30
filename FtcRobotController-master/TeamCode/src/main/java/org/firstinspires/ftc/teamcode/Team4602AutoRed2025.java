79cf8b97906fabd0abe44f7891243cf68cb4a556
Nobody <nobodyhere618@gmail.com>
2024-09-29 19:06:46 -0500

    added red auto template

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team4602AutoRed2025.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team4602AutoRed2025.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Team4602AutoRed2025 {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous (name="Team4602Auto1ParkingRed", group="4602")
+public class Team4602AutoRed2025 extends LinearOpMode{
+
+    Team6976HM2025 robot = new Team6976HM2025();
+    ElapsedTime Time = new ElapsedTime();
+    double multy = 0.3;
+
+    @Override
+    public void runOpMode() {
+        robot.Map(hardwareMap);
+        waitForStart();
+
+        //write code here
+
+
+    }
+
 }
